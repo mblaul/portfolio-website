41c3571accaabe7d510aca4c81653f48de6d40f7
mblaul <32026169+mblaul@users.noreply.github.com>
2018-06-28 05:21:41 -0400

    Working on colors and basic css

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,35 +1,37 @@
 import React, { Component } from "react";
 
 class Content extends Component {
-	render() {
-		return (
-			<div>
-				<div className="jumbotron">
-					<div className="row">
-						<div className="col-6">
-							<section className="about">
-								<h1>Matthew Blaul</h1>
-								<p>
-									<ul>
-										<li>Full Stack Web Developer</li>
-										<li>Data Analyst</li>
-										<li>Animal Lover</li>
-									</ul>
-								</p>
-							</section>
-						</div>
-						<div className="col-6">
-							<img
-								src="https://learn.extension.org/assets/avatar_placeholder-4c003c71739be2ed78864bcd49550380.png"
-								alt="Avatar"
-							/>
-						</div>
-					</div>
-				</div>
-				<div className="container-fluid">TEST</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <div className="jumbotron">
+          <div className="row">
+            <div className="col-6">
+              <section id="about">
+                <h1>Matthew Blaul</h1>
+                <p>
+                  <ul>
+                    <li>Full Stack Web Developer</li>
+                    <li>Data Analyst</li>
+                    <li>Animal Lover</li>
+                  </ul>
+                </p>
+              </section>
+            </div>
+            <div className="col-6">
+              <img
+                src="https://learn.extension.org/assets/avatar_placeholder-4c003c71739be2ed78864bcd49550380.png"
+                alt="Avatar"
+                width="250"
+                height="250"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="container-fluid">TEST</div>
+      </div>
+    );
+  }
 }
 
 export default Content;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 
 class Navbar extends Component {
-	render() {
-		return (
-			<div>
-				<nav className="navbar navbar-light bg-light align-left">Home</nav>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <nav className="navbar align-left">Home</nav>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
