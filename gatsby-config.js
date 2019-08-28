69073a96610db92a302d993c45b5d0c9486ef06f
mblaul <mattblaul@gmail.com>
2019-08-28 19:58:21 -0400

    Adding sanity files to project

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         //icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'portfolio-website',
+        dataset: 'portfolio-website',
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
