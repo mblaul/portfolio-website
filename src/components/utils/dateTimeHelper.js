c766a41cbf5e23f692043c0230991891c9433cac
mblaul <mattblaul@gmail.com>
2020-04-21 20:29:29 -0400

    Scrapping all old code, starting fresh Adding sanity + next dependencies

--- src/components/utils/dateTimeHelper.js
+++
@@ -1,4 +0,0 @@
-export function formatDate(date) {
-  const locale = window.navigator.userLanguage || window.navigator.language;
-  return new Intl.DateTimeFormat(locale, { dateStyle: 'long', timeStyle: 'short' }).format(date);
-}
